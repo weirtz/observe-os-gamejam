53f5abc1d42371b1d28aaa87fd92ad251667f9e2
Seabass247 <sebjfk@gmail.com>
2018-05-21 16:12:45 -0400

    Mouse click

--- CursorControl.cs
+++ CursorControl.cs
@@ -7,12 +7,13 @@ public class CursorControl : Control
     // private int a = 2;
     // private string b = "textvar";
     public Vector2 pos;
+    private Vector2 rectScale;
 
     public override void _Ready()
     {
         // Called every time the node is added to the scene.
         // Initialization here
-        
+        rectScale = RectScale;
     }
 
     //    public override void _Process(float delta)
@@ -24,5 +25,10 @@ public class CursorControl : Control
     public override void _Process(float delta)
     {
         SetPosition(pos);
+        if (Input.IsActionJustPressed("ui_select"))
+            RectScale = RectScale / (new Vector2((float)1.1, (float)1.1));
+        if (Input.IsActionJustReleased("ui_select"))
+            RectScale = this.rectScale;
+
     }
 }
